954888a815a67054d77b51cf1d6935a8957a68b1
Abdo Mohammad <abdomhammad618@gmail.com>
2024-02-18 11:27:12 +0200

    creating navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import Image from 'next/image'
-import Logo from '@/../public/Images/Logo.png'
+import Logo from '@/../public/Images/Logo.svg'
 import BurgerMenu from '@/../public/Images/burger-menu.png'
+import BurgerMenuSvg from '@/../public/Images/Group 59.svg'
 const NavBar = () => {
   return (
     <nav className='flex  items-center justify-center p-10 bg-backgroundColor'>
      
         <Image src={Logo} width={120} height={120} alt={'Logo'}  />
         <button  className='absolute right-5'>
-        <Image src={BurgerMenu} width={35} height={35} alt='burger-menu' />
+        <Image src={BurgerMenuSvg} width={50} height={50} alt='burger-menu' className='rounded-lg p-3 bg-primaryColor' />
       </button>
     </nav>
   )
